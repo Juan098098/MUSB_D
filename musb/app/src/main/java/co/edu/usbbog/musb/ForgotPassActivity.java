4f35625da63bfb7666effe92f0a753d360d5a0f0
Ncaron2020 <121366368+Ncaron2020@users.noreply.github.com>
2023-02-14 19:07:57 -0500

    [Infraestructura]:Se sube proyecto Android

---
+++ musb/app/src/main/java/co/edu/usbbog/musb/ForgotPassActivity.java
@@ -0,0 +1,15 @@
+package co.edu.usbbog.musb;
+
+import android.Manifest;
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class ForgotPassActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+    }
+}
